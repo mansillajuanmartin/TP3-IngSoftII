5c8c5a44915ae2d5147d8b18c7f4a0524f831fba
Lucas <lucasferr6@gmail.com>
2021-09-07 23:09:20 -0300

    Corrigo PHP y agrego Total

--- formulario.php
+++ formulario.php
@@ -2,7 +2,10 @@
     $precio1 = $_GET ['precio1'];
     $precio2 = $_GET ['precio2'];
     $precio3 = $_GET ['precio3'];
-    $media = ($precio1+$precio2+$precio3)/3;
+    $precio4 = $_GET ['precio4'];
+    $precio5 = $_GET ['precio5'];
+    $Total = ($precio1+$precio2+$precio3+$precio4+$precio5);
+    $media = ($Total)/5;
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +13,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
-        <title>Precio medio de un producto</title>
+        <title>Total y Precio medio de un producto</title>
     </head>
     <body>
     <?php
@@ -18,6 +21,9 @@
     echo "<p> Precio del producto del local 1: ". $precio1. " Pesos. </p>";
     echo "<p> Precio del producto del local 2: ". $precio2. " Pesos. </p>";
     echo "<p> Precio del producto del local 3: ". $precio3. " Pesos. </p>";
+    echo "<p> Precio del producto del local 4: ". $precio3. " Pesos. </p>";
+    echo "<p> Precio del producto del local 5: ". $precio5. " Pesos. </p>";
+    echo "<p> Precio Total es: ". $Total. " Pesos. </p>";
     echo "<p> Precio medio del producto es: ". $media. " Pesos. </p>";
     ?>
     </body>
